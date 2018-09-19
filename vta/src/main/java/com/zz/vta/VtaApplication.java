08a2d8ea2c1b52c0ec1e9b98967ee4ced5e2a9d5
yanggang <31473199@qq.com>
2018-09-19 08:47:16 +0800

    new zzvta

---
+++ vta/src/main/java/com/zz/vta/VtaApplication.java
@@ -0,0 +1,12 @@
+package com.zz.vta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VtaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VtaApplication.class, args);
+    }
+}
